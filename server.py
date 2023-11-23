67460913b504dbce194842ae97442da430597297
Mikeyp1234 <51434401+MikeyP1234@users.noreply.github.com>
2023-11-22 22:22:45 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 import json
 import random
-from datetime import datetime
+import datetime
 import hashlib
 import sys
 keyDict={}
@@ -13,13 +13,14 @@ def parseMessage(m):
     sum=0
     for header in lines[1:]:
         if header=="":
-            break #reached body
+            break 
         sum=sum+1
         hkey,hval = header.split(": ",1)
         headers[hkey] = hval
     return method,target,version,headers
 def postRequest(headers,jsonFile,version):
-    cdt=datetime.now()
+    cdt2=datetime.datetime.now().timestamp()
+    cdt=datetime.datetime.now()
     cdtb=cdt.strftime("%Y-%m-%d-%H-%M-%S")
     if 'username' not in headers or 'password' not in headers:
         print("SERVER LOG: "+cdtb+" LOGIN FAILED")
@@ -28,12 +29,12 @@ def postRequest(headers,jsonFile,version):
     if access==1:
         num=hex(random.getrandbits(64))
         sessionID="sessionID="+num
-        keyDict[sessionID]=headers['username'],cdtb
+        keyDict[num]=headers['username'],cdt2
         print("SERVER LOG: "+cdtb+" LOGIN SUCCESSFUL: "+headers['username']+" : "+headers['password'])
-        return version+" 200 OK\r\nSet-Cookie: "+sessionID+"\r\n\r\nLogged In!\r\n"
+        return version+" 200 OK\r\nSet-Cookie: "+sessionID+"\r\n\r\nLogged In!"
     else:
         print("SERVER LOG: "+cdtb+" LOGIN FAILED: "+headers['username']+" : "+headers['password'])
-        return version+" 200 OK\r\n\r\nLogin Failed!\r\n"
+        return version+" 200 OK\r\n\r\nLogin Failed!"
 def authenticatePassword(username,password,jsonFile):
     sha256=hashlib.sha256()
     with open(jsonFile) as json_file:
@@ -50,9 +51,11 @@ def authenticatePassword(username,password,jsonFile):
 def getRequest(headers,target,seshTimeout,rootDirectory,version):
     if 'Cookie' not in headers:
         return version+" 401 Unauthorized\r\n\r\n"
-    cdt=datetime.now()
+    cdt2=datetime.datetime.now().timestamp()
+    cdt=datetime.datetime.now()
     cdtb=cdt.strftime("%Y-%m-%d-%H-%M-%S")
     cookie=str(headers['Cookie'])
+    id, cookie=cookie.split("=")
     cookie=cookie.replace('/r','')
     cookie=cookie.strip()
     if cookie not in keyDict:
@@ -62,20 +65,19 @@ def getRequest(headers,target,seshTimeout,rootDirectory,version):
         cookiedata=keyDict[cookie]
         user=str(cookiedata[0])
         cookietime=cookiedata[1]
-        t1=datetime.strptime(cookietime,"%Y-%m-%d-%H-%M-%S")
-        t2=datetime.strptime(cdtb,"%Y-%m-%d-%H-%M-%S")
+        t1=cookietime
+        t2=cdt2 
         tdelta=t2-t1
         seshion=int(seshTimeout)
-        td=int(tdelta.total_seconds())
-        if td<=seshion:
-            keyDict[cookie]=user,cdtb
+        if tdelta<=seshion:
+            keyDict[cookie]=user,cdt2
             search=rootDirectory+user+target
             search=search.strip()
             try:
                 f=open(search,"r")
                 message=f.read()
                 print("SERVER LOG: "+cdtb+" GET SUCCEEDED: "+user+" : "+target)
-                return version+" 200 OK\r\n\r\n"+message+"\r\n"
+                return version+" 200 OK\r\n\r\n"+message
             except IOError:
                 print("SERVER LOG: "+cdtb+" GET FAILED: "+user+" : "+target)
                 return version+" 404 NOT FOUND\r\n\r\n"
@@ -83,16 +85,16 @@ def getRequest(headers,target,seshTimeout,rootDirectory,version):
         else:
             print("SERVER LOG: "+cdtb+" SESSION EXPIRED "+user+" : "+target)
             del keyDict[cookie]
-            return version+" 401 Unauthorized"+"\r\n\r\n"
+            return version+" 401 Unauthorized\r\n\r\n"
         
     
     
 def startServer(IPaddress,portNum,jsonFile,seshTimeout,rootDirectory):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ip=str(IPaddress)
+    ip=IPaddress
     pN=int(portNum)
     server_socket.bind((ip, pN))
-    server_socket.listen(5)
+    server_socket.listen()
     while True:
         client_socket, client_address = server_socket.accept()
         hello_msg = client_socket.recv(1024).decode()
@@ -105,9 +107,8 @@ def startServer(IPaddress,portNum,jsonFile,seshTimeout,rootDirectory):
             msg=getRequest(headers,target,seshTimeout,rootDirectory,version)
             client_socket.sendall(msg.encode())
         else:
-            client_socket.sendall(version+" 501 Not Implemented"+"\r\n\r\n".encode())
+            client_socket.sendall((version+" 501 Not Implemented"+"\r\n\r\n").encode())
         client_socket.close()
         
 if __name__ == '__main__':
     startServer(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5])
-    
